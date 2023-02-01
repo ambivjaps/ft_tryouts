fb4ab0145e5c0b6b9dd651c6de2278ec9d7bc9c2
James Sing <87142332+ambivjaps@users.noreply.github.com>
2023-02-02 05:07:43 +0800

    Made front-end design for form

--- resources/views/pages/form.blade.php
+++ resources/views/pages/form.blade.php
@@ -1,1 +1,126 @@
-FLIPTOP GAME
+@extends('layouts.app')
+
+@section('title')
+<title> Registration Form | {{ setting('site.title') }} </title>
+@endsection
+
+@section('meta')
+<meta property="og:url"                content="https://tryouts.fliptop.com.ph/form" />
+<meta property="og:title"              content="{{ setting('site.title') }}" />
+<meta property="og:description"        content="{{ setting('site.description') }}" />
+<meta property="og:image"              content="{{ URL::asset('/img/bg_og.jpg') }}" />
+<meta name="twitter:card"              content="summary_large_image">
+<meta name="description"               content="{{ setting('site.description') }}" />
+<meta name="keywords"                  content="FlipTop, FlipTop Battle League, Filipino Rap Battle, Rap, Rap Battle, Hip-Hop, Filipino" />
+@endsection
+
+@section('content')
+
+<div class="container-xxl my-5">
+    <div class="ft-bcrumb">
+        <small>
+            <ol class="breadcrumb" style="--bs-breadcrumb-divider: '>';">
+                <li class="breadcrumb-item"><a href="{{ URL::asset('/') }}"><i class="fas fa-home"></i></a></li>
+                <li class="breadcrumb-item active" aria-current="page">Registration Form</li>                
+            </ol>
+        </small>
+    </div><hr>
+
+    <form class="row g-3">
+
+        <h3> Personal Information </h3>
+        <div class="col-md-4">
+            <label for="firstName" class="form-label">First Name</label>
+            <input type="text" class="form-control" placeholder="(ex. Juan)">
+        </div>
+
+        <div class="col-md-4">
+            <label for="lastName" class="form-label">Last Name</label>
+            <input type="text" class="form-control" placeholder="(ex. Dela Cruz)">
+        </div>
+
+        <div class="col-md-4">
+            <label for="alias" class="form-label">Alias/Emcee Name</label>
+            <input type="text" class="form-control" placeholder="(ex. MC Hotdog)">
+        </div>
+
+        <div class="col-md-4">
+            <label for="reppin" class="form-label">Reppin/Affiliations</label>
+            <input type="text" class="form-control" placeholder="Enter your crew/affiliations here.">
+        </div>
+
+        <div class="col-md-4">
+            <label for="birthday" class="form-label">Birthday</label>
+            <input type="date" class="form-control">
+        </div>
+
+        <div class="col-md-4">
+            <label for="division" class="form-label">Division</label>
+            <select id="division" class="form-select">
+            <option selected>Select division</option>
+            <option>Metro Manila</option>
+            <option>Calabarzon</option>
+            <option>Central Luzon</option>
+            <option>Visayas</option>
+            <option>Mindanao</option>
+            </select>
+        </div>
+        
+        <div class="col-12">
+            <label for="address" class="form-label">Address</label>
+            <input type="text" class="form-control" placeholder="(ex. Gangsterville, Mandaluyong)">
+        </div>
+
+        <div class="col-md-4">
+            <label for="cred_email" class="form-label">E-mail Address</label>
+            <input type="email" class="form-control" placeholder="Enter your e-mail address here.">
+        </div>
+
+        <div class="col-md-4">
+            <label for="cred_fb" class="form-label">Facebook Account</label>
+            <input type="text" class="form-control" placeholder="Enter your Facebook account link here.">
+        </div>
+
+        <div class="col-md-4">
+            <label for="cred_cp" class="form-label">Cellphone No.</label>
+            <input type="text" class="form-control" placeholder="Enter your cellphone number here.">
+        </div><hr>
+
+        <h3> Lyrics </h3>
+        
+        <div class="col-md-6">
+            <label for="exampleFormControlTextarea1" class="form-label">Lyrics (16 Bars for Song)</label>
+            <textarea class="form-control" id="exampleFormControlTextarea1" rows="10" placeholder="Can be same as video link."></textarea>
+        </div>
+
+        <div class="col-md-6">
+            <label for="exampleFormControlTextarea1" class="form-label">Lyrics (16 Bars for Imaginary Battle of FlipTop Emcee)</label>
+            <textarea class="form-control" id="exampleFormControlTextarea1" rows="10" placeholder="Can be same as video link."></textarea>
+        </div><hr>
+
+        <h3> Video Links </h3>
+
+        <div class="col-md-6">
+            <label for="link_song" class="form-label">Video Link (16 Bars for Song)</label>
+            <input type="text" class="form-control" placeholder="Use #FlipTopPOI7.">
+        </div>
+
+        <div class="col-md-6">
+            <label for="link_battle" class="form-label">Video Link (16 Bars for Battle, Acapella)</label>
+            <input type="text" class="form-control" placeholder="Use #FlipTopPOI7.">
+        </div>
+
+        <div class="col-md-12">
+            <label for="link_others" class="form-label">Video Links (Past Music & Battles)</label>
+            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Enter relevant links here."></textarea>
+        </div><hr>
+
+        <div class="col-12">
+            <button type="submit" class="btn btn-lg btn-warning float-end my-3">Submit Application</button>
+        </div>
+
+    </form>
+
+</div>
+
+@endsection
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -17,15 +17,24 @@
 @section('content')
 
 <div class="container-xxl my-5">
-    <div class="h-100 p-5 text-white bg-dark rounded-2 ft-card-ins" style="background-image: linear-gradient(to bottom, rgba(0,0,0,0.7) 0%,rgba(0,0,0,0.9) 100%), url('img/battles2.jpg');">
+    <div class="h-100 p-5 text-white bg-dark rounded-2 ft-poi-ins">
         <div class="row">
             <div class="col-md-4">
-                <img src="img/POI7_3.jpg" class="rounded" width="100%">
+                <img src="{{ URL::asset('img/POI7_3.jpg') }}" class="rounded" width="100%">
             </div>
             <div class="col-md-8">
                 <h1 class="my-3 lh-1" style="text-shadow: #000 1px 0 6px;"> #FlipTopPOI7 </h1><hr>
-                <i> Submissions have ended. </i> <p> Stay tuned for upcoming announcements! </p>
-                <a class="btn btn-warning mt-3" href="https://www.fliptop.com.ph" role="button">Back to main website</a>
+                <p>A. Submission of requirements for the First Leg of the Tryouts will be accepted on March 29, 2018.</p>
+                <p>B. Requirements:</p>
+                <ol>
+                    <li>Fill up required information.</li>
+                    <li>Prepare (2) sets of 16 Bars for Battle against any imaginary opponent.</li>
+                    <li>Prepare (1) set of 16 Bars for a Song (any topic).</li>
+                    <li>Prepare Audio/Video links of all previous battles, songs, and live performances.</li>
+                </ol>
+                <p>C. Cut off for submissions is on April 28, 2018.</p>
+                <p>D. Stay tuned for details and updates on the Second Leg of the Tryouts - Regional Tournaments!</p>
+                <a class="btn btn-lg btn-warning mt-3" href="{{ URL::asset('/form') }}" role="button">Apply for Tryouts</a>
             </div>
         </div>
     </div>
