2139b4ec5f08b8a5bd7f0f02c41537979a483549
James Sing <87142332+ambivjaps@users.noreply.github.com>
2023-02-03 01:39:49 +0800

    Revised fields

--- resources/views/pages/form.blade.php
+++ resources/views/pages/form.blade.php
@@ -21,52 +21,30 @@
         <small>
             <ol class="breadcrumb" style="--bs-breadcrumb-divider: '>';">
                 <li class="breadcrumb-item"><a href="{{ URL::asset('/') }}"><i class="fas fa-home"></i></a></li>
-                <li class="breadcrumb-item active" aria-current="page">Registration Form</li>                
+                <li class="breadcrumb-item active" aria-current="page">Step#1 - Personal Information</li>                
             </ol>
         </small>
     </div><hr>
 
     <form class="row g-3">
 
-        <h3> Personal Information </h3>
-        <div class="col-md-4">
+        <h3> Step#1 - Personal Information </h3>
+        <div class="col-md-6">
             <label for="firstName" class="form-label">First Name</label>
             <input type="text" class="form-control" placeholder="(ex. Juan)">
         </div>
 
-        <div class="col-md-4">
+        <div class="col-md-6">
             <label for="lastName" class="form-label">Last Name</label>
             <input type="text" class="form-control" placeholder="(ex. Dela Cruz)">
         </div>
 
-        <div class="col-md-4">
-            <label for="alias" class="form-label">Alias/Emcee Name</label>
-            <input type="text" class="form-control" placeholder="(ex. MC Hotdog)">
-        </div>
-
-        <div class="col-md-4">
-            <label for="reppin" class="form-label">Reppin/Affiliations</label>
-            <input type="text" class="form-control" placeholder="Enter your crew/affiliations here.">
-        </div>
-
-        <div class="col-md-4">
+        <div class="col-md-6">
             <label for="birthday" class="form-label">Birthday</label>
             <input type="date" class="form-control">
         </div>
 
-        <div class="col-md-4">
-            <label for="division" class="form-label">Division</label>
-            <select id="division" class="form-select">
-            <option selected>Select division</option>
-            <option>Metro Manila</option>
-            <option>Calabarzon</option>
-            <option>Central Luzon</option>
-            <option>Visayas</option>
-            <option>Mindanao</option>
-            </select>
-        </div>
-        
-        <div class="col-12">
+        <div class="col-md-6">
             <label for="address" class="form-label">Address</label>
             <input type="text" class="form-control" placeholder="(ex. Gangsterville, Mandaluyong)">
         </div>
@@ -86,37 +64,8 @@
             <input type="text" class="form-control" placeholder="Enter your cellphone number here.">
         </div><hr>
 
-        <h3> Lyrics </h3>
-        
-        <div class="col-md-6">
-            <label for="exampleFormControlTextarea1" class="form-label">Lyrics (16 Bars for Song)</label>
-            <textarea class="form-control" id="exampleFormControlTextarea1" rows="10" placeholder="Can be same as video link."></textarea>
-        </div>
-
-        <div class="col-md-6">
-            <label for="exampleFormControlTextarea1" class="form-label">Lyrics (16 Bars for Imaginary Battle of FlipTop Emcee)</label>
-            <textarea class="form-control" id="exampleFormControlTextarea1" rows="10" placeholder="Can be same as video link."></textarea>
-        </div><hr>
-
-        <h3> Video Links </h3>
-
-        <div class="col-md-6">
-            <label for="link_song" class="form-label">Video Link (16 Bars for Song)</label>
-            <input type="text" class="form-control" placeholder="Use #FlipTopPOI7.">
-        </div>
-
-        <div class="col-md-6">
-            <label for="link_battle" class="form-label">Video Link (16 Bars for Battle, Acapella)</label>
-            <input type="text" class="form-control" placeholder="Use #FlipTopPOI7.">
-        </div>
-
-        <div class="col-md-12">
-            <label for="link_others" class="form-label">Video Links (Past Music & Battles)</label>
-            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Enter relevant links here."></textarea>
-        </div><hr>
-
         <div class="col-12">
-            <button type="submit" class="btn btn-lg btn-warning float-end my-3">Submit Application</button>
+            <button type="submit" class="btn btn-lg btn-warning float-end my-3">Next </i></button>
         </div>
 
     </form>
