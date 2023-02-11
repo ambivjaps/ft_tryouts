107d3cfe12805776358d86d90631cffc6f67db11
James Sing <87142332+ambivjaps@users.noreply.github.com>
2023-02-11 15:25:15 +0800

    Added closed blade

---
+++ resources/views/pages/closed.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.app')
+
+@section('title')
+<title> Home | {{ setting('site.title') }} </title>
+@endsection
+
+@section('meta')
+<meta property="og:url"                content="https://tryouts.fliptop.com.ph" />
+<meta property="og:title"              content="{{ setting('site.title') }}" />
+<meta property="og:description"        content="{{ setting('site.description') }}" />
+<meta property="og:image"              content="{{ URL::asset('/img/bg_og.jpg') }}" />
+<meta name="twitter:card"              content="summary_large_image">
+<meta name="description"               content="{{ setting('site.description') }}" />
+<meta name="keywords"                  content="FlipTop, FlipTop Battle League, Filipino Rap Battle, Rap, Rap Battle, Hip-Hop, Filipino" />
+@endsection
+
+@section('content')
+
+<div class="container-xxl my-5">
+
+    <div class="h-100 p-5 text-white bg-dark rounded-2 ft-poi-ins">
+        <div class="row">
+            <div class="col-md-5">
+                <img src="{{ URL::asset('img/POI7_remix.png') }}" class="rounded img-fluid">
+            </div>
+            <div class="col-md-7">
+                <h1 class="my-3 lh-1" style="text-shadow: #000 1px 0 6px;"> #FlipTopPOI7Remix </h1><hr>
+                <p> Submissions have ended. Stay tuned for details and updates on the Second Leg of the Tryouts - Regional Tournaments! </p>
+                <a class="btn btn-warning mt-3" href="https://www.fliptop.com.ph" role="button">Back to main website</a>
+            </div>
+        </div>
+    </div>
+    <div class="my-5">
+        <center>
+            <iframe class="embedvid" width="100%" height="600" src="https://www.youtube.com/embed/UK0twoDs5pY" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </center>
+    </div>
+</div>
+
+@endsection
