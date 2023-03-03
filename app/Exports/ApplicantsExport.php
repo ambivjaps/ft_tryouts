bf235a8df1e4f5383a9c3a1d5d8cb230a8d9f0db
James Sing <87142332+ambivjaps@users.noreply.github.com>
2023-03-04 07:38:40 +0800

    Added headings

--- app/Exports/ApplicantsExport.php
+++ app/Exports/ApplicantsExport.php
@@ -4,8 +4,9 @@ namespace App\Exports;
 
 use App\Models\Applicant;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class ApplicantsExport implements FromCollection
+class ApplicantsExport implements FromCollection, WithHeadings
 {
     /**
     * @return \Illuminate\Support\Collection
@@ -13,5 +14,31 @@ class ApplicantsExport implements FromCollection
     public function collection()
     {
         return Applicant::all();
+    }
+
+    public function headings(): array
+    {
+        return [
+            'Applicant ID',
+            'First Name',
+            'Last Name',
+            'Emcee Name/Alias',
+            'Reppin',
+            'Division',
+            'Birthday',
+            'Hometown',
+            'E-mail Address',
+            'Facebook Acct',
+            'Cellphone No.',
+            '16 Bars (Song)',
+            '16 Bars (Battle)',
+            'Link (Song)',
+            'Link (Battle)',
+            'Link (Others)',
+            'Status',
+            'Created At',
+            'Updated At',
+        ];
+
     }
 }
